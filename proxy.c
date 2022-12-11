c84b2c9a86e61e3c9bddb6f259c70bc14c3f5e88
choihs0457 <85881597+choihs0457@users.noreply.github.com>
2022-12-12 00:23:45 +0900

    branch add

--- proxy.c
+++ proxy.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+// 다녀감
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
@@ -9,7 +9,8 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-int main() {
+int main()
+{
   printf("%s", user_agent_hdr);
   return 0;
 }
