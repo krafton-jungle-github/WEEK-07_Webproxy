996f83656a857c183e2d2bd96f6149693e14f2c6
hoondal69 <davidtwins6970@gmail.com>
2022-12-12 22:23:33 +0900

    doit함수

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -41,3 +41,46 @@ int main(int argc, char **argv) {
     Close(connfd);  // line:netp:tiny:close
   }
 }
+
+void doit(int fd)
+{
+  int is_static;
+  struct stat sbuf;
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  char filename[MAXLINE], cgiargs[MAXLINE];
+  rio_t rio;
+  /* Read request line and headers*/
+  Rio_readinitb(&rio, fd);
+  Rio_readlineb(&rio, buf, MAXLINE);
+  printf("REquest headers: \n");
+  printf("%s", buf);
+  sscanf(buf, "%s %s %s", method, uri, version);
+  if (strcasecmp(method, "GET")){
+    clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+    return;
+  }
+  read_requesthdrs(&rio);
+  
+
+  /* Parse URI from GET request*/
+  is_static = parse_uri(uri, filename, cgiargs);
+  if (stat(filename, &sbuf) < 0){
+    clienterror(fd, filename, "404", "Not found", "Tiny couldn't find this file");
+    return;
+  }
+
+  if (is_static) {/* Serve static content*/
+      if (! (S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+        clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+        return;
+      }
+      serve_static(fd, filename, sbuf.st_size);
+  }
+  else { /* Serve dynamic content */
+      if (! (S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+        clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
+        return;
+      }
+      serve_dynamic(fd, filename, cgiargs);
+  }
+}
